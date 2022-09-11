eb613f0b481b47179819a2bb1fa76919cc5e94b3
abed <abod>
2022-09-11 17:46:39 +0300

    add-packges

---
+++ lib/Screens/MainScreen/DitailsScreen/DitailsScreen.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class DitailsScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+     return Scaffold(
+
+     );
+  }
+
+}
