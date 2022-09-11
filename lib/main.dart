b5769a3b71b9d0ee2e329b78b72f24c84dda8bcd
abed <abod>
2022-09-11 18:40:13 +0300

    asd

--- lib/main.dart
+++ lib/main.dart
@@ -24,8 +24,19 @@ class MyApp extends StatelessWidget {
         textDirection: TextDirection.ltr,
         
         child: MaterialApp(
-          title: 'Flutter Demo',
-          home: DitailsScreen(),
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+             scaffoldBackgroundColor:  Colors.white,
+            accentColor: Colors.transparent,
+            iconTheme: IconThemeData(
+              color: Colors.grey[600],
+size: 22
+            ),
+
+
+          ),
+          themeMode: ThemeMode.light,
+          home: DitailsScreen(imageUrl: "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",),
         ),
       );}
     );
